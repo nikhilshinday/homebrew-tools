689af6f415e93f360065414ff1129a2821b6f3c5
Nikhil Shinday <nikhilshinday@gmail.com>
2026-02-09 05:01:33 -0500

    gw: use v0.1.0 release tarball

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -1,9 +1,8 @@
 class Gw < Formula
   desc "Git worktree helper for managing parallel sandboxes"
   homepage "https://github.com/nikhilshinday/gw"
-  url "https://github.com/nikhilshinday/gw/archive/932c6d0.tar.gz"
-  sha256 "62ca2437a6748e2309d084fa941fe4aeb08e01ecfe99a8fa71c686095b325ef6"
-  version "0.1.0"
+  url "https://github.com/nikhilshinday/gw/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "49f397216b6140bdffd381962bbcc7182be12b6a971d3fc278085805bed21a85"
 
   depends_on "rust" => :build
 
@@ -12,6 +11,6 @@ class Gw < Formula
   end
 
   test do
-    system "#{bin}/gw", "--help"
+    system bin/"gw", "--help"
   end
 end
